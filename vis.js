c2e53b71f9a8dfe9540bca8a7e4af3b512a164ba
vishnubabulal <157651416+vishnubabulal@users.noreply.github.com>
2024-02-09 00:00:49 +0100

    Javascript code for showing infinite data visualization using data sets

---
+++ vis.js
@@ -0,0 +1,224 @@
+var nodes = null;
+var edges = null;
+var network = null;
+var colID = null; //variable for node click ID
+var assetsDataArray = []; //array for data set
+//Data array starts here
+var assetsDataArray = {
+  "51":
+  {"AssetLinks":{"51":{"links":{"116":"Fileserver","10193":"Test Asset"},"services":{"11":"service00","125":"IT service 39","481":"service1001"},"dataProcesses":{"20":"Data Test1"},"departments":{"10013":"PDF-Export","10014":"SystemData" ,"2911":"ITAsset111 "}}}},
+  "116":
+  {"AssetLinks":{"116":{"links":{"51":"Contract 144","10193":"Test Asset"},"services":{"11":"service00","5960":"IT-Service01"},"dataProcesses":{"5902":"Data01"}}}},
+  "2911":
+  {"AssetLinks":{"2911":{"links":{"51":"Contract 144","10193":"Test Asset"},"services":{"594":"service1004","6964":"ITService02","6967":"ITService03"}}}}
+}
+//Data array ends here
+var nodes = new vis.DataSet(); 
+var edges = new vis.DataSet();
+if(assetsDataArray != ''){ 
+  callDataArray(); //called function to retrive the array data
+}
+//function for array call starts here
+function callDataArray(colID = '', toggle = ''){ 
+  if(colID != '' && assetsDataArray.hasOwnProperty(colID) ){
+    toggle = !toggle;
+  }else{
+    toggle = false;
+  } 
+  var arrayDataNodesCount =  Object.keys(assetsDataArray).length; //count of array
+  jQuery.each(assetsDataArray, function(assetid) { 
+    if(colID != ''){
+      if(colID == assetid) {
+        assetid = !colID; //taking the selected asset
+      } else {
+        assetid = colID;
+      }
+    } else {
+      var initialID = Object.keys(assetsDataArray)[0]; //console.log(initialID+'-'+ assetid);
+    }
+    var WIDTH_SCALE = 0.5;
+    var arrayDataNodesCount =  Object.keys(assetsDataArray).length; //count of array
+    if(arrayDataNodesCount <= 2 )
+      var lengthEdge = 130;
+    else
+      var lengthEdge = (arrayDataNodesCount * 130);
+    var assetsLinksData = assetsDataArray[assetid];
+    jQuery.each(assetsLinksData, function(assetLink) { 
+      var assetsLinksObjects = assetsLinksData[assetLink];
+      jQuery.each(assetsLinksObjects, function(assetnodeKey) { 
+        var assetsTypeObjects = assetsLinksObjects[assetnodeKey];
+        var nodesObjCount = Object.keys(assetsTypeObjects).length;
+        if(nodesObjCount <= 3)
+          var LENGTH_MAIN = 120; 
+        else
+          var LENGTH_MAIN = (nodesObjCount*50); 
+        jQuery.each(assetsTypeObjects, function(assetTypeName) { 
+          var assetsTypeValues = assetsTypeObjects[assetTypeName]; 
+          if(colID != ''){
+            var nodeLength = Object.keys(assetsTypeValues).length*70;  
+          }
+          //console.log(assetid +'-'+Object.keys(assetsTypeValues)[0]);
+          switch(assetTypeName)
+          {
+            case 'links':
+              var shapeIconlabel = '\uf109';
+              var shapeIconColor = '#10b2fe';
+              break;
+            case 'services':
+              var shapeIconlabel = '\uf233';
+              var shapeIconColor = '#690e6d';
+              break;
+            case 'dataProcesses':
+              var shapeIconlabel = '\uf28d';
+              var shapeIconColor = '#000000';
+              break;
+            case 'departments':
+              var shapeIconlabel = '\uf19c';
+              var shapeIconColor = '#f19814';
+              break;
+            case 'networks':
+              var shapeIconlabel = '\uf19c';
+              var shapeIconColor = '#ff1493';
+              break;
+            case 'vtResponsibles':
+              var shapeIconlabel = '\uf19c';
+              var shapeIconColor = '#2d8c0d';
+              break;
+            case 'vtProcessors':
+              var shapeIconlabel = '\uf1e0';
+              var shapeIconColor = '#808000';
+              break;
+            case 'bProcesses':
+              var shapeIconlabel = '\uf1e0';
+              var shapeIconColor = '#ffd700';
+              break;
+            case 'itProcesses':
+              var shapeIconlabel = '\uf1e0';
+              var shapeIconColor = '#00fa9a';
+              break;
+            case 'informationCategories':
+              var shapeIconlabel = '\uf19c';
+              var shapeIconColor = '#ffa07a';
+              break;  
+            case 'tasks':
+              var shapeIconlabel = '\uf14a';
+              var shapeIconColor = '#ff4500';
+              break;
+            case 'default':
+              var shapeIconlabel = '\uf03a';
+              var shapeIconColor = '#800000';
+              break;                       
+          }
+          jQuery.each(assetsTypeValues, function(assetsTypeValueId,typeLabel) {
+            //console.log(assetid +'-'+ assetsTypeValueId);
+            let drawitem = true; 
+            var nodeArray = nodes.get(nodes[0]); //checking duplicate ids from nodes
+            $.each(nodeArray, function(nodesid,nodesitem){  
+              if(nodesitem.value == assetsTypeValueId && colID == '') {
+                //console.log(nodesitem.value +'-'+ assetsTypeValueId);
+                drawitem = false; 
+              }
+              else if(colID == assetsTypeValueId && colID != '') {
+                //console.log(nodesitem.value +'-'+ assetsTypeValueId);
+                drawitem = false; 
+              } 
+            });  //console.log(drawitem+'-'+assetid +'-'+ assetsTypeValueId);
+            if(drawitem) {
+              if(colID != ''){ 
+                if(assetsDataArray.hasOwnProperty(assetsTypeValueId) && toggle == true) {
+                  assetsTypeValueId = null;
+                } 
+
+                if(assetsDataArray.hasOwnProperty(assetsTypeValueId)){
+                  var nodeColor = '#153c65'; //asset color to identify
+                  var fontColor = '#153c65';
+                  var physicsSet = true;
+                  var lengthSet = lengthEdge;
+                  var iconSize = 50;
+                } else {
+                  var nodeColor = shapeIconColor;
+                  var fontColor = '#153c65';
+                  var physicsSet = null;
+                  var lengthSet = LENGTH_MAIN;
+                  var iconSize = 50;
+                }
+                //console.log(toggle+'-'+assetid +'-'+ assetsTypeValueId);
+                nodes.update([
+                  {id: assetsTypeValueId, title:assetTypeName, label: typeLabel, value: assetsTypeValueId,hidden:toggle,physics:physicsSet , shape:'icon',icon: { face: "'FontAwesome'", code: shapeIconlabel, size: iconSize, color: nodeColor }, font: { color:fontColor}}, 
+                ]); 
+                edges.update([
+                  {id:assetsTypeValueId,from: colID, to: assetsTypeValueId,hidden:toggle, smooth: false,fixed:true, length: lengthSet }, 
+                ]);
+              } 
+              if(colID == '') {
+                if( assetsDataArray.hasOwnProperty(assetsTypeValueId) ){
+                  nodes.update([
+                    {id: assetsTypeValueId, title:assetTypeName, label: typeLabel, value: assetsTypeValueId,hidden:toggle,physics:false, shape:'icon',icon: { face: "'FontAwesome'", code: shapeIconlabel, size: 50, color: '#153c65' }, font:{ color:'#153c65'} }, 
+                  ]); 
+                }
+                edges.update([
+                  {id:assetsTypeValueId,from: assetid, to: assetsTypeValueId, smooth: false,hidden:toggle,color:'#153c65', width: WIDTH_SCALE * 3, length: lengthEdge}, 
+                ]);
+              }
+            }
+          }); 
+        });
+      });
+    });
+  });
+}
+//function for array call ends here
+var containerShow = document.getElementById('assetsVisJs');
+var dataDrawCategories = {
+  nodes: nodes,
+  edges: edges
+};
+var options = {
+  edges: {
+    color: '#153c65',
+    arrows: {
+      //to: { enabled:true, scaleFactor:0.5, type: 'arrow'},
+    }
+  },
+  interaction: { hover: true },
+  physics: {
+    forceAtlas2Based: {
+      gravitationalConstant: -15,
+      centralGravity: 0.005,
+      springLength: 120,
+      springConstant: 0.18,
+      avoidOverlap: 1.5
+    },
+    maxVelocity: 12,
+    timestep: 0.35,
+    stabilization: {
+      enabled: true,
+      iterations: 1000,
+      updateInterval: 1
+    }
+  }
+};
+var network = new vis.Network(containerShow, dataDrawCategories, options);
+//code to disable the physics 
+network.on("stabilizationIterationsDone", function () {
+  network.setOptions( { physics: true } );
+});
+//code to disable the physics 
+//hovernode & blur node to change the cursor while moving over the nodes
+network.on("hoverNode", function (params) {
+  network.canvas.body.container.style.cursor = 'pointer'
+});
+network.on("blurNode", function (params) {
+  network.canvas.body.container.style.cursor = 'default'
+});
+var toggle = true; // true=hide; false=show
+network.on("click", function(e) { //console.log(toggle);
+  var nodeCollapseArray = nodes.get(e.nodes[0]); //alert(JSON.stringify(nodeCollapseArray));
+  //console.log(nodeCollapseArray);
+  if(e.nodes[0] != '' && e.nodes[0] != undefined){
+    var clickCalID = e.nodes[0]; 
+    callDataArray(clickCalID,toggle); //function call to retrieve assets nodes
+  }
+  //network.fit();
+  toggle = !toggle; // switching toggle
+});
